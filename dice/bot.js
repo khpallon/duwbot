1ade86a9cd0452bf0163f5aaeb3a12ffa2e55c68
khpallon <karlhannespallon@gmail.com>
2023-06-18 16:50:23 +0000

    bot responds to specific person who answered correctly, removed useless code lines

--- dice/bot.js
+++ dice/bot.js
@@ -1,6 +1,5 @@
 const tmi = require('tmi.js');
 
-// Define configuration options
 const opts = {
   identity: {
     username: 'duw_bot',
@@ -23,48 +22,35 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+client.on('connected', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`hor`);
+});
+
+
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
-  console.log(msg.trim() === anser)
-  console.log(msg)
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-
-  } else if (commandName === '!quiz') {
+   if (commandName === '!quiz') {
 
     var [eq, ans] = equation();
     anser.push(ans)
-    console.log(anser[0])
     client.say(target, `Solve this equation!: ${eq}`); 
 
    } else if (parseInt(msg) === anser[0]){
 
-    client.say(target, `Correct answer!`)
+    client.say(target, `${context['display-name']} solved the equation!`)
     anser = []
 
-   } else {
-
-    console.log(`* Unknown command ${commandName}`);
-
-  }
+   }
 }
 
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
 
 const equation = () => {
-
     var first = Math.floor(Math.random() * 10)
     var second = Math.floor(Math.random() * 10)
     return [`${first} * ${second}`, first * second]
